f56e87bf24ce267f00161ddae8973cf4312a76fe
yay12 <shockblaster9@yahoo.com>
2025-02-27 18:26:18 +0000

    Optimize dependencies - fixing typo

--- resources/monitorDescription.js
+++ resources/monitorDescription.js
@@ -2,13 +2,13 @@
 
 import Gtk from "gi://Gtk";
 import Gdk from "gi://Gdk";
-//const { Gtk, Gdk } = imports.gi;
+//let { Gtk, Gdk } = imports.gi;
 
 Gtk.init();
-const monitors = Gdk.Display.get_default().get_monitors();
-const details = [];
-for (const m of monitors) {
-    const { x, y, width, height } = m.get_geometry();
+let monitors = Gdk.Display.get_default().get_monitors();
+let details = [];
+for (let m of monitors) {
+    let { x, y, width, height } = m.get_geometry();
     details.push({ name: m.get_description(), x, y, width, height });
 }
 
